e3c93c269126154d7eb2c3f16509364893dd489a
NhuPTQ <quynhnhuctcn@gmail.com>
2022-04-23 01:15:23 +0700

    update menu

---
+++ amai-fe/src/app/component/user/sinup-user/sinup-user.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sinup-user',
+  templateUrl: './sinup-user.component.html',
+  styleUrls: ['./sinup-user.component.css']
+})
+export class SinupUserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
