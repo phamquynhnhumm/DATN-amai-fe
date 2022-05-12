c25cb85ad78fb91ae0b16b942a450f6c323d7d48
NhuPTQ <quynhnhuctcn@gmail.com>
2022-05-12 23:40:26 +0700

    update delte food

--- amaife/src/app/component/admin/content/food/deletefood/deletefood.component.ts
+++ amaife/src/app/component/admin/content/food/deletefood/deletefood.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
+import {FoodService} from "../../../../../service/food.service";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {Food} from "../../../../../model/food/Food";
 
 @Component({
   selector: 'app-deletefood',
@@ -6,10 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./deletefood.component.scss']
 })
 export class DeletefoodComponent implements OnInit {
+  food!: Food;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private dialogRef: MatDialogRef<DeletefoodComponent>,
+              private foodService: FoodService,
+              @Inject(MAT_DIALOG_DATA) public data: any,
+              private snackBar: MatSnackBar
+  ) {
   }
 
+  ngOnInit(): void {
+    this.food = this.data;
+  }
+
+  deletefood() {
+    this.foodService.deleteByIdFood(this.food.id).subscribe(() => {
+      this.dialogRef.close();
+      this.snackBar.open("Xoá món thành công !!! ", "OK", {
+        duration: 4000
+      })
+    })
+  }
 }
