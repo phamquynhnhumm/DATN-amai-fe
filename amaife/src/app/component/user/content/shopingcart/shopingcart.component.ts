ddfcd5bd2e05c42f91721bad1c746153980feef1
NhuPTQ <quynhnhuctcn@gmail.com>
2022-05-22 01:19:19 +0700

    update shooping

---
+++ amaife/src/app/component/user/content/shopingcart/shopingcart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shopingcart',
+  templateUrl: './shopingcart.component.html',
+  styleUrls: ['./shopingcart.component.scss']
+})
+export class ShopingcartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- amaife/src/app/component/user/user-routing.module.ts
+++ amaife/src/app/component/user/user-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import {HomeuserComponent} from "./content/homeuser/homeuser.component";
 import {MenuuserComponent} from "./content/menuuser/menuuser.component";
 import {DetailfooduserComponent} from "./content/detailfooduser/detailfooduser.component";
+import {ShopingcartComponent} from "./content/shopingcart/shopingcart.component";
 
 const routes: Routes = [
   {path:"home",component:HomeuserComponent},
   {path:"menu",component:MenuuserComponent},
+  {path:"shoping",component:ShopingcartComponent},
   {path:"detailfood/:id",component:DetailfooduserComponent}
 ];
 
