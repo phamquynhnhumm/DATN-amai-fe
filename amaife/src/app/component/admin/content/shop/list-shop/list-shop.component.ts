fd23fc8eb7ae2731d944a97976d53aa616b74ed5
NhuPTQ <quynhnhuctcn@gmail.com>
2022-06-03 08:37:16 +0700

    update list shop

--- amaife/src/app/component/admin/content/shop/list-shop/list-shop.component.ts
+++ amaife/src/app/component/admin/content/shop/list-shop/list-shop.component.ts
@@ -3,7 +3,7 @@ import {ShopService} from "../../../../../service/shop.service";
 import {FoodService} from "../../../../../service/food.service";
 import {SupplierService} from "../../../../../service/supplier.service";
 import {ActivatedRoute} from "@angular/router";
-import {FormBuilder} from "@angular/forms";
+import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {MatSnackBar} from "@angular/material/snack-bar";
 import {MatDialog} from "@angular/material/dialog";
 import {Shop} from "../../../../../model/shop/Shop";
@@ -29,8 +29,31 @@ export class ListShopComponent implements OnInit {
       data => {
         for (let sp of data) {
           this.shop = data[0];
+          console.log(this.shop)
         }
       }
     )
+  }
+
+  formShop = new FormGroup(
+    {
+      address: new FormControl(this.shop.address, [Validators.required, Validators.minLength(2), Validators.maxLength(255)]),
+      nameShop: new FormControl(this.shop.nameShop, [Validators.required, Validators.minLength(1), Validators.maxLength(15)]),
+      phone: new FormControl(this.shop.phone, [Validators.required, Validators.min(0)]),
+      slogen: new FormControl(this.shop.slogen, [Validators.required, Validators.min(0)]),
+      nameTransfer: new FormControl(this.shop.nameTransfer, [Validators.required, Validators.minLength(2), Validators.maxLength(255)]),
+      bankName: new FormControl(this.shop.bankName, Validators.required),
+      number: new FormControl(this.shop.number, Validators.required),
+      facebook: new FormControl(this.shop.facebook, Validators.required),
+      youtube: new FormControl(this.shop.youtube, Validators.required),
+      content: new FormControl(this.shop.content, Validators.required),
+      instagram: new FormControl(this.shop.instagram, Validators.required),
+      logo: new FormControl(this.shop.logo, Validators.required),
+      email: new FormControl(this.shop.email, Validators.required),
+    }
+  )
+
+  onSubmit() {
+
   }
 }
